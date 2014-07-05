07bd166f9b973cb453baf2d8c24e76aa7f02ede2
Marco <marcooliveira@infojr.com.br>
2014-07-05 15:48:20 -0300

    novo

--- trabalho.asm
+++ trabalho.asm
@@ -1,28 +1,48 @@
 %include "asm_io.inc"
 
 section .data
-vetor dd 0,1,2,1,2,1,2,4,3,3,4,2
+matrix dd 0,1,2,1,2,1,2,4,3,3,4,2
 
 section .bss
+MAX_TAM resd 1
 
 section .text
 
 global asm_main
 asm_main:
 
-mov ecx,0 ;iterador
-mov edx, 20 ;ultima posicao a ser verificada
-mov eax,[vetor] ;serve para buscar informacoes no vetor
-percorre:
-	cmp ecx,edx
-	je sair
-	cmp [vetor+ecx], eax
-	jne sair
-	add ecx, 4
-	mov eax, [vetor+ecx]
-	add ecx, 8
-jmp percorre
-sair:
+; Deve ser removido no futuro para dar espaço a pegar informações do teclado para preencher a matriz
+mov edx, 36 ;ultima posicao a ser verificada (dado por tamanho total do vetor - 4)
+mov MAX_TAM, [edx]
 
+mov ebx,[matrix] ;valor da posição da matriz
+mov ecx,0 ;valor do indice da matriz
+
+
+; A lógica a partir daqui permanece
+push ebx
+push ecx
+call search
 leave
 ret
+
+;Transformar isso em função recursiva:
+search:
+	push ebp
+	mov ebp, esp
+	mov ebx, [ebp+4]
+	mov ecx, [ebp+8]
+	
+	cmp [ecx],MAX_TAM
+	je exit
+	cmp [matrix+ecx], eax
+	jne exit
+	add ecx, 4
+	mov eax, [matrix+ecx]
+	add ecx, 8
+	jmp search
+exit:
+
+pop ebp
+ret
+;======================================
