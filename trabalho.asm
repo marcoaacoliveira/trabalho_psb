209912a9713508d46b73bf7465ab532fe609b052
Hiolanda <hiolandamurielle@infojr.com.br>
2014-07-05 13:19:37 -0300

    add trabalho asm

--- trabalho.asm
+++ trabalho.asm
@@ -1,20 +1,28 @@
+%include "asm_io.inc"
+
 section .data
-vetor dd 0,1,2,1,2,1
+vetor dd 0,1,2,1,2,1,2,4,3,3,4,2
 
 section .bss
 
 section .text
 
-global main
+global asm_main
+asm_main:
 
-main:
-
-
+mov ecx,0 ;iterador
+mov edx, 20 ;ultima posicao a ser verificada
+mov eax,[vetor] ;serve para buscar informacoes no vetor
 percorre:
+	cmp ecx,edx
+	je sair
 	cmp [vetor+ecx], eax
 	jne sair
+	add ecx, 4
 	mov eax, [vetor+ecx]
-	add ecx, 12
-	call print_int
+	add ecx, 8
 jmp percorre
 sair:
+
+leave
+ret
